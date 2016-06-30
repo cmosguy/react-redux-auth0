e7b020ae0c9beec7bb1bea7e198a8904f597a016
Pavel Lang <langpavel@phpskelet.org>
2016-06-30 17:09:06 +0200

    Auth reducer cleanup. See note in getInitialState

--- src/reducers/authenticate.js
+++ src/reducers/authenticate.js
@@ -1,38 +1,48 @@
-import { combineReducers } from 'redux'
-
 import {
-    LOCK_SUCCESS, LOGOUT_SUCCESS,
-    QUOTE_REQUEST, QUOTE_SUCCESS, QUOTE_FAILURE
+  LOCK_SUCCESS,
+  LOGOUT_SUCCESS,
 } from '../actions/authenticate';
+
+function getInitialState() {
+  let isAuthenticated = false;
+  if (process.env.BROWSER) {
+    // NOTE: Using different state in browser when mounting component can trigger React warning:
+    // "React attempted to reuse markup in a container but the checksum was invalid"
+    // Possible solution is dispatch some special action after first render on client.
+    if (typeof localStorage !== 'undefined') {
+      isAuthenticated = !!localStorage.getItem('id_token');
+    }
+  }
+  return {
+    isFetching: false,
+    isAuthenticated,
+  };
+}
 
 // The auth reducer. The starting state sets authentication
 // based on a token being in local storage. In a real app,
 // we would also want a util to check if the token is expired.
-export default function auth(state = {
-    isFetching: false,
-    isAuthenticated: localStorage.getItem('id_token') ? true : false
-  }, action) {
+export default function auth(state = getInitialState(), action) {
   switch (action.type) {
-    case LOCK_SUCCESS:
-      return Object.assign({}, state, {
+    case LOCK_SUCCESS: {
+      return {
+        ...state,
         isFetching: false,
         isAuthenticated: true,
-        errorMessage: ''
-      });
-    case LOGOUT_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: true,
-        isAuthenticated: false
-      });
-    default:
-      return state
+        errorMessage: '',
+      };
     }
+
+    case LOGOUT_SUCCESS: {
+      return {
+        ...state,
+        isFetching: true,
+        isAuthenticated: false,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
 }
-
-// We combine the reducers here so that they
-// can be left split apart above
-// const quotesApp = combineReducers({
-//   auth,
-//   quotes
-// });
-
